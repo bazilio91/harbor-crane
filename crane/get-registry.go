affb8ba6b83b4cb63555674b5289c94475d9ae7d
Vasiliy Ostanin <bazilio@tochka.com>
2019-11-15 12:57:13 +0500

    rewrite registry ping part

--- crane/get-registry.go
+++ crane/get-registry.go
@@ -1,6 +1,8 @@
 package crane
 
 import (
+	"fmt"
+	"net/http"
 	"strings"
 
 	"github.com/heroku/docker-registry-client/registry"
@@ -23,7 +25,7 @@ func (c *Crane) GetRegistry(url string) (*registry.Registry, error) {
 		repo_url = "https://" + url
 	}
 
-	reg, err := registry.New(repo_url, creds.Username, creds.Password)
+	reg, err := createRegistry(repo_url, creds.Username, creds.Password, logrus.Debugf)
 
 	if err != nil {
 		logrus.WithError(err).Error("failed to create https repo, trying http")
@@ -37,14 +39,36 @@ func (c *Crane) GetRegistry(url string) (*registry.Registry, error) {
 		reg, err = registry.New(repo_url, creds.Username, creds.Password)
 
 		if err != nil {
-			// sometimes /v2/ is closed by auth
-			if !strings.Contains(err.Error(), "status=401") {
-				return nil, err
-			}
+			return nil, err
 		}
 	}
 
 	reg.Logf = logrus.Debugf
 
 	return reg, nil
+}
+
+func createRegistry(registryURL, username, password string, logf registry.LogfCallback) (*registry.Registry, error) {
+	transport := http.DefaultTransport
+
+	url := strings.TrimSuffix(registryURL, "/")
+	transport = registry.WrapTransport(transport, url, username, password)
+	registry := &registry.Registry{
+		URL: url,
+		Client: &http.Client{
+			Transport: transport,
+		},
+		Logf: logf,
+	}
+
+	resp, err := registry.Client.Get(fmt.Sprintf("%s%s", registry.URL, "/v2/_ping"))
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
+	if resp.StatusCode < 500 {
+		return registry, nil
+	}
+
+	return nil, err
 }
